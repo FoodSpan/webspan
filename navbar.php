a4deaff8d441012a15922afaec4e984ecbed9206
Matthew Wang <matthewang.wang@gmail.com>
2016-08-09 21:07:00 -0400

    creates basic page structure + import pages

---
+++ navbar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-container" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">WebSpan</a>
+    </div>
+
+    <div class="collapse navbar-collapse" id="navbar-container">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">App</a>
+        </li>
+        <li>
+          <a href="#">Blog</a>
+        </li>
+        <li>
+          <a href="#">Shop</a>
+        </li>
+        <li>
+          <a href="#">Team</a>
+        </li>
+        <li>
+          <a href="#">Contact Us</a>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="#" class="btn btn-rose btn-raised btn-round">
+            Register
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
